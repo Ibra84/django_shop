f639b2f5731fb67af49eab708180f5ea4c5506a6
Ibrohim <ibrohimdzonsultanov@gmail.com>
2024-05-19 14:41:13 +0500

    The main app is ready

--- main/views.py
+++ main/views.py
@@ -3,11 +3,34 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'The main page of shop Home'
+        'title': 'Главная',
+        'content': 'Магазин мебели HOME',
     }
     
     return render(request, 'main\index.html', context=context)
 
 def about(request):
-    return HttpResponse ('About page')
+    context = {
+        'title': 'About us',
+        'content': 'About us',
+        'text_on_page': 'Text about why this shop is the best and our goods are best im the world'
+    }
+    return render(request, 'main/about.html', context=context)
+ 
+def contacts(request):
+    context = {
+        'title': 'Contacts',
+        'content': 
+            'Phone: +992 93 594 54-65',
+            
+        'text_on_page': 'You can contact us when you need some help'
+    }
+    return render(request, 'main/contacts.html', context=context)
+
+def delivery(request):
+    context = {
+        'title': 'Delivery',
+        'content': 'Delivery',
+        'text_on_page': 'You can make an order here and pay for it'
+    }
+    return render(request, 'main/delivery.html', context=context)
