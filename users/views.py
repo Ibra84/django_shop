6b5b52ed720cf8b4131428afc9631ab060a745b5
Ibrohim <ibrohimdzonsultanov@gmail.com>
2024-05-30 22:53:22 +0500

    last edits

--- users/views.py
+++ users/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib import auth,messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch
 
 
+from orders.models import Order, OrderItem
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 def login(request):
@@ -60,10 +62,23 @@ def profile(request):
             return HttpResponseRedirect(reverse('user:profile'))
     else:
         form = ProfileForm(instance=request.user) 
+    
+    orders = (
+        Order.objects.filter(user=request.user).
+        prefetch_related(
+            Prefetch(
+                'orderitem_set',
+                queryset=OrderItem.objects.select_related('product'),
+            )
+        )
+        .order_by('-id')
+    )
+    
         
     context = {
         'title': 'Profile',
-        'form': form
+        'form': form,
+        'orders': orders
     }
     return render(request, 'users/profile.html', context=context)
     
